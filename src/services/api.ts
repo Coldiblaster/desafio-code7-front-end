d1f0dddd2aaa81efd46f15bab1de1d4d1bbdf3f0
Coldiblaster <vbastazin@gmail.com>
2020-11-09 11:52:14 -0300

    Alterando baseUrl api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://192.168.31.157:3333/api/v1',
+  baseURL: 'http://localhost:3333/api/v1',
 });
 
 export const apiExtern = axios.get(
