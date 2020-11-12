3031ede56c0246dcb0836bf45b61dc1db6ccb046
Coldiblaster <vbastazin@gmail.com>
2020-11-12 19:50:32 -0300

    Add route private

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Switch } from 'react-router-dom';
 
-import { Switch, Route } from 'react-router-dom';
+import Route from './Route';
 
 import Debt from '../pages/Debt';
 import SignIn from '../pages/SignIn';
+import SignUp from '../pages/Signup';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={Debt} />
-    <Route path="/signup" component={SignIn} />
+    <Route path="/" exact component={SignIn} />
+    <Route path="/signup" exact component={SignUp} />
+    <Route path="/debt" component={Debt} isPrivate />
   </Switch>
 );
 
